5a45ac579ed5946965cbd6ec14ea7add7386eb41
Paul Fahey <pfahey1@mail.plymouth.edu>
2013-11-05 15:26:11 -0500

    added keyboard and password functionality to project

--- alarm_project/alarm_project.ino
+++ alarm_project/alarm_project.ino
@@ -1,4 +1,4 @@
-#include <LiquidCrystal.h>
+
 
 // Uses a PIR sensor to detect movement, buzzes a buzzer
 // more info here: http://blog.makezine.com/projects/pir-sensor-arduino-alarm/
@@ -9,63 +9,59 @@
 
 // include the library code:
 
-LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
+#include <LiquidCrystal.h>
+#include <Password.h> 
+#include <Keypad.h> 
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 3; //  columns
+
+// Define the Keymap
+char keys[ROWS][COLS] = {
+{'1','2','3'},
+ {'4','5','6'},
+ {'7','8','9'},
+ {'*','0','#'}
+};
+
+byte rowPins[ROWS] = { 5, 4, 3, 2 };// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 8, 7, 6 };
+
  
 int ledPin = 13;                // choose the pin for the LED
-int inputPin = 8;               // choose the input pin (for PIR sensor)
+int inputPin = 9;               // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
 int pinSpeaker = 10;           //Set up a speaker on a PWM pin (digital 9, 10, or 11)
 
+LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
+Password password = Password( "1234" );
+
+// Create the Keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+boolean disarm = password.evaluate();
+
 void setup() 
 {
+  keypad.addEventListener(keypadEvent); //add an event listener for this keypad
+  
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
   pinMode(pinSpeaker, OUTPUT);
-  lcd.begin(16, 2);
-   //lcd.print("hello, world!");
- // Serial.begin(9600);
+  
+  lcd.begin(16, 2); 
+  lcd.print("Type Password");
+  lcd.setCursor(0,1);
+  lcd.print("To Arm System");
  }
 
 void loop()
-{
-     lcd.setCursor(0, 1);
-    val = digitalRead(inputPin);  // read input value
+{   
     
-  if (val == HIGH) 
-  {            // check if the input is HIGH
-    digitalWrite(ledPin, HIGH);  // turn LED ON
-    playTone(300, 160);
-    delay(150);
-
-    if (pirState == LOW) 
-    {
-      // we have just turned on
-      //Serial.println("Motion detected!");
-      lcd.print("Motion detected!");
+     keypad.getKey(); 
       
-      // We only want to print on the output change, not state
-      pirState = HIGH;
-    }
-  } 
-  
-  else 
-    {
-      digitalWrite(ledPin, LOW); // turn LED OFF
-      playTone(0, 0);
-      delay(300);    
-      if (pirState == HIGH)
-      {
-      // we have just turned of
-     // Serial.println("Motion ended!");
-      lcd.print("Motion finished!");
-      delay(5000);
-      lcd.clear();
-      // We only want to print on the output change, not state
-      pirState = LOW;
-    }
-  }
-}
+} //end of main loop
 
 // duration in mSecs, frequency in hertz
 void playTone(long duration, int freq) 
@@ -83,3 +79,93 @@ void playTone(long duration, int freq)
         elapsed_time += (period);
     }
 }
+
+void keypadEvent(KeypadEvent eKey)
+{
+  switch (keypad.getState())
+  {
+    case PRESSED:
+	//lcd.print("Pressed: ");
+        lcd.clear();
+        lcd.setCursor(0,1);
+	lcd.print(eKey);
+	switch (eKey)
+        {
+	  case '*': checkPassword(); break;
+          
+	  case '#': password.reset(); break;
+	  default: password.append(eKey);
+        }
+  }
+}
+
+void checkPassword()
+{
+  
+  if (password.evaluate())
+  {
+    lcd.print("Success!");
+    delay(3000);
+    lcd.clear();
+    lcd.print("System Armed"); 
+   
+    //Add code to run if it works
+    while (password.evaluate())
+    
+    {
+      setOffSystem();
+      
+      if(disarm)
+      {
+        break;
+      }
+    }
+        
+  }    
+  
+  else
+  {
+    lcd.print("Wrong Password");
+    delay(5000);
+    lcd.clear();
+    //add code to run if it did not work
+  }
+}
+
+void setOffSystem()
+{
+   val = digitalRead(inputPin);  // read input value
+    
+  if (val == HIGH) 
+  {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    playTone(300, 160);
+    delay(150);
+
+    if (pirState == LOW) 
+    {
+      // we have just turned on
+      lcd.clear();
+      lcd.print("Motion detected!");
+      
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } 
+  
+  else 
+    {
+      digitalWrite(ledPin, LOW); // turn LED OFF
+      playTone(0, 0);
+      delay(300);    
+      if (pirState == HIGH)
+      {
+      lcd.print("Motion finished!");
+      delay(5000);
+      lcd.clear();
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }       
+}
+
